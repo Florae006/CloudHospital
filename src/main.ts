05896786af36d481b5f1b12849c28287b0fe5592
flora <flora_chen2021@163.com>
2025-02-24 00:03:04 +0800

    main.ts router, style.css

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,71 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+import router from './router';
+
+import ElementPlus from 'element-plus';
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import {
+  faLocation,
+  faPeopleGroup,
+  faCheck,
+  faHand,
+  faUser,
+  faRightFromBracket,
+  faHouse,
+  faClipboard,
+  faClock,
+  faIdCard,
+  faShuffle,
+  faRotate,
+  faUserTie,
+  faUserGraduate,
+  faList,
+  faPlus,
+  faMagnifyingGlassPlus,
+  faCircleCheck,
+  faCircleXmark,
+  faFile,
+  faQuestionCircle,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(
+  faLocation,
+  faPeopleGroup,
+  faCheck,
+  faHand,
+  faUser,
+  faRightFromBracket,
+  faHouse,
+  faClipboard,
+  faClock,
+  faIdCard,
+  faShuffle,
+  faRotate,
+  faUserTie,
+  faUserGraduate,
+  faList,
+  faPlus,
+  faMagnifyingGlassPlus,
+  faCircleCheck,
+  faCircleXmark,
+  faXmark,
+  faFile,
+  faQuestionCircle,
+);
+
+createApp(App).use(pinia).use(router).use(ElementPlus).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,83 @@
+import { createRouter } from 'vue-router';
+import { createWebHistory } from 'vue-router';
+
+const routes = [];
+
+routes.push({
+  name: 'notFound',
+  path: '/:pathMatch(.*)*',
+  component: () => import('@/views/NotFound/NotFound.vue'),
+});
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// 路由护卫
+
+router.beforeEach((to, from, next) => {
+  console.log('to:', to);
+  console.log('from:', from);
+  next();
+  // if (to.path === '/logout') {
+  //   localStorage.clear();
+  //   if (from.path.startsWith('/admin')) {
+  //     console.log('admin logout');
+  //     next('/admin/login');
+  //   } else {
+  //     next('/login');
+  //   }
+  //   return;
+  // }
+
+  // if (to.path === '/login' || to.path === '/admin/login') {
+  //   localStorage.clear();
+  //   next();
+  //   return;
+  // }
+  // if (!to.meta.requireAuth || from.meta.requireAuth) {
+  //   next();
+  //   return;
+  // }
+  // const notLogin = useAccessTokenStore().getAccessToken() === '';
+  // if (notLogin) {
+  //   ElMessageBox.confirm('请先登录', '提示', {
+  //     confirmButtonText: '确定',
+  //     type: 'warning',
+  //   }).then(() => {
+  //     if (to.path.startsWith('/admin')) {
+  //       next('/admin/login');
+  //     } else {
+  //       next('/login');
+  //     }
+  //   });
+  //   return;
+  // }
+  // // next();
+  // commonOnline()
+  //   .then((response) => {
+  //     const res = response.data;
+  //     if (res.code === 0) {
+  //       next();
+  //     } else {
+  //       ElMessage.error(res.message);
+  //       if (to.path.startsWith('/admin')) {
+  //         next('/admin/login');
+  //       } else {
+  //         next('/login');
+  //       }
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     ElMessage.error('网络错误');
+  //     if (to.path.startsWith('/admin')) {
+  //       next('/admin/login');
+  //     } else {
+  //       next('/login');
+  //     }
+  //   });
+});
+
+export default router;
